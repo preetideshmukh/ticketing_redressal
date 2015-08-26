1613a059fee35a569e6e521940fa1b1b0b5a8220
Preeti Deshmukh <prtdshmkh068@gmail.com>
2015-08-26 10:28:27 +0530

    second commit

--- app/email_receivers/incoming.rb
+++ app/email_receivers/incoming.rb
@@ -1,6 +1,7 @@
 class EmailToSmsReceiver < Incoming::Strategies::Postmark
   def receive(mail)
-    send_sms([mail.subject, mail.body].join(": "))
+    #send_sms([mail.subject, mail.body].join(": "))
+  	puts "IN TO THE EmailToSmsReceiver"
   end
   private
     def send_sms(message)
