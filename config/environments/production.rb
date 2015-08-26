36f6572e08c2478819cfb6e203cc707da8a22f04
Preeti Deshmukh <prtdshmkh068@gmail.com>
2015-08-26 17:16:23 +0530

    new commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
+   ActionMailer::Base.delivery_method=:smtp
+ ActionMailer::Base.smtp_settings = {
+     :address =>"smtp.gmail.com",
+     :port => 587,
+     :domain => "gmail.com",
+     :user_name => "anoopzakasboy@gmail.com",
+     :password => "password@2281131",
+     :authentication => :plain,
+     :enable_starttls_auto => true,
+     :openssl_verify_mode => 'none' 
+     }
+
 end
