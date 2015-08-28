ba35c018989fd0d1ceb21d98079cf007628ef1b2
Preeti Deshmukh <prtdshmkh068@gmail.com>
2015-08-28 15:34:17 +0530

    Added changes in application.rb

--- config/application.rb
+++ config/application.rb
@@ -23,13 +23,14 @@ ActionMailer::Base.smtp_settings = {
     ENV[key] = value.to_s unless value.kind_of? Hash
   end
 end
+
 module ComplaintSystem
   class Application < Rails::Application
     
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
