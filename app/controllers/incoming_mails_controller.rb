c039a75e6b46dc7d467e865acaddccf4ccc920b6
Preeti Deshmukh <prtdshmkh068@gmail.com>
2015-08-31 14:05:00 +0530

    controller changed

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -5,8 +5,10 @@ skip_before_filter :authenticate_user!
 
   def create
     message = Mail.new(params[:message])
-    Rails.logger.log Logger::INFO, message.subject #print the subject to the logs
-    Rails.logger.log Logger::INFO, message.body.decoded #print the decoded body to the logs
+    
+    Ticket.create(:user_id=>current_user.id,:crn=>message.subject,:description=>message.body.decoded)
+    # Rails.logger.log Logger::INFO, message.subject #print the subject to the logs
+    # Rails.logger.log Logger::INFO, message.body.decoded #print the decoded body to the logs
 
     # Do some other stuff with the mail message
 
